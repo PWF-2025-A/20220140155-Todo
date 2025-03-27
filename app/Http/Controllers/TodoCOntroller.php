4affbaf2c36c534a8232905b9e5bfe0974ae4ae1
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-03-27 16:09:21 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoCOntroller.php
+++ app/Http/Controllers/TodoCOntroller.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class TodoCOntroller extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        // $todos = Todo::all();
+        $todos = Todo::where('user_id', Auth::id())->get();
+        dd($todos);
         return view('todo.index');
     }
 
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'is_done'];
 }
