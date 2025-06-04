234d544fd0bcda9f625a5795f467b21a90d4908e
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-06-05 02:24:43 +0700

    api : auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Providers;
+
+use Dedoc\Scramble\Scramble;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-
+use Laravel\Sanctum\Sanctum;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +31,9 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useTailwind();
         Gate::define('admin', function($user) {
             return $user->is_admin == true;
+        });
+        Scramble::configure()->routes(function (Route $route) {
+            return Str::startsWith($route->uri, 'api/');
         });
     }
 }
---
+++ tugas11.md
@@ -0,0 +1,2 @@
+Login
+![alt text](<screnshoot/tugas11/Screenshot 2025-06-05 022313.png>)
