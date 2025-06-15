b82986e2825e67e7119aee9b873b7c9bd73f571a
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-06-16 02:45:41 +0700

    api : logout, create, update, delete data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,12 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Sanctum\Sanctum;
-use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,11 +29,19 @@ class AppServiceProvider extends ServiceProvider
         // Todo::preventLazyLoading();
 
         Paginator::useTailwind();
-        Gate::define('admin', function($user) {
+
+        Gate::define('admin', function ($user) {
             return $user->is_admin == true;
         });
-        Scramble::configure()->routes(function (Route $route) {
-            return Str::startsWith($route->uri, 'api/');
-        });
+
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            })
+            ->withDocumentTransformers(function (OpenApi $openApi): void {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            });
     }
 }
---
+++ tugas12.md
@@ -0,0 +1,19 @@
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 022737.png>)
+
+Logout dan menghapus JW
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 022929.png>)
+
+Create Data
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 023629.png>)
+
+Read Data
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 023558.png>)
+
+Search Data
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 023540.png>)
+
+Update Data
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 023846.png>)
+
+Delete Data
+![alt text](<screnshoot/tugas12/Screenshot 2025-06-16 023905.png>)
